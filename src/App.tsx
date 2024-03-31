1c1bc9188da0a3fa7675b426854dff45f11c5ecf
vakabunga <shesterov@gmail.com>
2024-03-31 12:44:48 +0300

    added state to App, added back "browserlist" to package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { ProgressBar } from './components/ProgressBar/ProgressBar';
+import { SuccessfullImage } from './components/SuccessfullImage/SuccessfullImage';
+
 import './App.css';
 
 const App = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+
   return (
     <div className="App">
+      <ProgressBar isLoaded={isLoaded} onLoaded={setIsLoaded} />
+      {isLoaded && <SuccessfullImage />}
     </div>
   );
-}
+};
 
 export { App };
